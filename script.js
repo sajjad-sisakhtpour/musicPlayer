1552a4bd5e265b136445d471590f491f8353fe34
sajjad <sajjad.sisakhtpour@gmail.com>
2024-02-11 08:27:46 +0330

    added keys in js for easly coding

---
+++ script.js
@@ -0,0 +1,6 @@
+let musicName = document.querySelector("#music-name");
+let musicCover = document.querySelector("#music-cover");
+let range = document.querySelector("#music-time");
+let preBtn = document.querySelector("#pre-btn");
+let nextBtn = document.querySelector("#next-btn");
+let platBtn = document.querySelector("#play-btn");
