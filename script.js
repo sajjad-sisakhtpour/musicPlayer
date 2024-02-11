ad4994f7dd4527702b6ca03868ce90d249fc7880
sajjad <sajjad.sisakhtpour@gmail.com>
2024-02-11 08:31:06 +0330

    added a audio in js

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ let range = document.querySelector("#music-time");
 let preBtn = document.querySelector("#pre-btn");
 let nextBtn = document.querySelector("#next-btn");
 let platBtn = document.querySelector("#play-btn");
+let audio = document.createElement("audio");
+
+audio.src = "musics/kabk.mp3";
+musicName.innerText = "kabk";
+musicCover.src = "imgs/kabk.jpg";
