af16a58c41d5f93bf7a9d9814e837b5fedf56f9d
sajjad <sajjad.sisakhtpour@gmail.com>
2024-02-11 09:00:06 +0330

    added play button js codes

--- script.js
+++ script.js
@@ -3,9 +3,21 @@ let musicCover = document.querySelector("#music-cover");
 let range = document.querySelector("#music-time");
 let preBtn = document.querySelector("#pre-btn");
 let nextBtn = document.querySelector("#next-btn");
-let platBtn = document.querySelector("#play-btn");
+let playBtn = document.querySelector("#play-btn");
 let audio = document.createElement("audio");
 
 audio.src = "musics/kabk.mp3";
 musicName.innerText = "kabk";
 musicCover.src = "imgs/kabk.jpg";
+
+playBtn.addEventListener("click", () => {
+  if (audio.paused) {
+    audio.play();
+    musicCover.style.animationPlayState = "running";
+    playBtn.classList.replace("fa-pause", "fa-play");
+  } else {
+    audio.pause();
+    musicCover.style.animationPlayState = "paused";
+    playBtn.classList.replace("fa-play", "fa-pause");
+  }
+});
